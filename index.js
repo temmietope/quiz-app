82b252a7ab4ed6ffe2007beff97cb9261966022d
Temitope <temmieayodele@gmail.com>
2019-05-05 21:12:49 +0100

    changed UI, fetched questions from a data file and added Next functionality

--- index.js
+++ index.js
@@ -1,35 +1,101 @@
-const questionHeader = document.querySelector("#questionHeader");
-const theQuestion = document.querySelector("#theQuestion");
-const theOptions = document.querySelector("#theOptions");
+var currentQuestion = 0;
+var score = 0;
+var totalQuestion = questions.length;
 
-const btn = document.querySelector(".btn")
+var quizContainer = document.querySelector("#quizContainer")
+var theQuestion = document.querySelector("#question");
+var opt1 = document.querySelector("#opt1");
+var opt2 = document.querySelector("#opt2");
+var opt3 = document.querySelector("#opt3");
+var opt4 = document.querySelector("#opt4");
+var nextbtn = document.querySelector("#nextbtn");
+var resultContainer = document.querySelector("#result");
 
-count = 0;
-const options = questions[count].options;
-const question = questions[count].question;
-const answer = questions[count].answer;
-
-questionHeader.innerHTML = `QUESTION ${count + 1}`;
-theQuestion.innerHTML = question;
-
-//to display options
-
-options.forEach(function(el,index) {
-  let option = document.createElement("input");
-  option.type = "radio";
-  option.value = el;
-  option.name = el;
+function loadQuestion(questionIndex){
+  var q = questions[questionIndex];
+  theQuestion.innerHTML = `${questionIndex + 1}. ${q.question}`
+  opt1.innerHTML = q.options[0];
+  opt2.innerHTML = q.options[1];
+  opt3.innerHTML = q.options[2];
+  opt4.innerHTML = q.options[3];
+};
+loadQuestion(currentQuestion)
 
 
-  let list = document.createElement('li')
 
-  list.innerHTML = el;
-  
-  theOptions.appendChild(list);
-  list.appendChild(option)
-});
 
-btn.onclick = function(){
-  options[answer]
-  document.get
+nextbtn.onclick = function(){
+  currentQuestion+=1
+  loadQuestion(currentQuestion);
+  if (currentQuestion == (totalQuestion - 1)){
+    nextbtn.innerHTML = `Finish`
+  }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const questionHeader = document.querySelector("#questionHeader");
+// const theQuestion = document.querySelector("#theQuestion");
+// const theOptions = document.querySelector("#theOptions");
+
+// const btn = document.querySelector(".btn")
+
+// count = 0;
+// const options = questions[count].options;
+// const question = questions[count].question;
+// const answer = questions[count].answer;
+
+// questionHeader.innerHTML = `QUESTION ${count + 1}`;
+// theQuestion.innerHTML = question;
+
+// //to display options
+
+// options.forEach(function(el,index) {
+//   let option = document.createElement("input");
+//   option.type = "radio";
+//   option.value = el;
+//   option.name = 'optionName';
+
+
+//   let list = document.createElement('li')
+
+//   list.innerHTML = el;
+  
+//   theOptions.appendChild(list);
+//   list.appendChild(option)
+// });
+
+// btn.onclick = function(){
+//   options[answer]
+//   document.getE
+// }
--- questions.js
+++ questions.js
@@ -2,19 +2,19 @@ const questions=[
    {
         "id":1,
         "question":"Who is the president of Nigeria?",
-        "options":["Umar Musa Yar'adua", "Muhammed Buhari","Sheu Shagari"],
+        "options":["Umar Musa Yar'adua", "Muhammed Buhari","Sheu Shagari","No idea"],
         "answer": 1
     },
     {
         "id":2,
         "question":"Where is Tilapia Resort in Akwa Ibom?",
-        "options":["Uyo", "Ikot Ekpene","Abak"],
+        "options":["Uyo", "Ikot Ekpene","Abak", "No idea"],
         "answer": 2
     },
     {
         "id":3,
         "question":"What continent is nigeria in?",
-        "options":["Africa", "Asia","North America"],
+        "options":["Africa", "Asia","North America", "No idea"],
         "answer": 0
     }
 ]
