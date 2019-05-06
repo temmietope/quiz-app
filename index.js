ddd59d6ab920a5cf8b07fc645311c5e5fbcc36e7
Temitope <temmieayodele@gmail.com>
2019-05-06 21:06:57 +0100

    added load next question function and update of the score per question

--- index.js
+++ index.js
@@ -2,100 +2,62 @@ var currentQuestion = 0;
 var score = 0;
 var totalQuestion = questions.length;
 
-var quizContainer = document.querySelector("#quizContainer")
+var quizContainer = document.querySelector("#quizContainer");
 var theQuestion = document.querySelector("#question");
 var opt1 = document.querySelector("#opt1");
 var opt2 = document.querySelector("#opt2");
 var opt3 = document.querySelector("#opt3");
 var opt4 = document.querySelector("#opt4");
 var nextbtn = document.querySelector("#nextbtn");
+var finalbtn = document.querySelector("#finalbtn");
 var resultContainer = document.querySelector("#result");
 
-function loadQuestion(questionIndex){
+function loadQuestion(questionIndex) {
   var q = questions[questionIndex];
-  theQuestion.innerHTML = `${questionIndex + 1}. ${q.question}`
+  theQuestion.innerHTML = `${questionIndex + 1}. ${q.question}`;
   opt1.innerHTML = q.options[0];
   opt2.innerHTML = q.options[1];
   opt3.innerHTML = q.options[2];
   opt4.innerHTML = q.options[3];
-};
-loadQuestion(currentQuestion)
+}
+loadQuestion(currentQuestion);
 
+function loadNextQuestion() {
+  let radios = document.querySelectorAll("input[name='option']");
+  let correctAnswer = questions[currentQuestion].answer;
 
+  for (var i = 0, radioLength = radios.length; i < radioLength; i++) {
+    if (radios[i].checked) {
+      
+      console.log(radios[i].value);
+      console.log(correctAnswer);
 
-
-nextbtn.onclick = function(){
-  currentQuestion+=1
-  loadQuestion(currentQuestion);
-  if (currentQuestion == (totalQuestion - 1)){
-    nextbtn.innerHTML = `Finish`
+      if (correctAnswer === i) {
+        console.log("correct answer");
+        score += 1;
+      }
+      currentQuestion += 1;
+      loadQuestion(currentQuestion);
+    }
   }
 }
 
+function loadResult() {
+  quizContainer.style.display = "none";
+  resultContainer.style.display = "block";
+  resultContainer.textContent = `Your score is ${score} / 3`;
+  console.log(score);
+}
 
+nextbtn.onclick = function() {
+  if (currentQuestion < totalQuestion - 1) {
+    loadNextQuestion();
+  }
 
+  if (currentQuestion == totalQuestion - 1) {
+    nextbtn.style.display = "none";
+    finalbtn.style.display = "";
+  }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const questionHeader = document.querySelector("#questionHeader");
-// const theQuestion = document.querySelector("#theQuestion");
-// const theOptions = document.querySelector("#theOptions");
-
-// const btn = document.querySelector(".btn")
-
-// count = 0;
-// const options = questions[count].options;
-// const question = questions[count].question;
-// const answer = questions[count].answer;
-
-// questionHeader.innerHTML = `QUESTION ${count + 1}`;
-// theQuestion.innerHTML = question;
-
-// //to display options
-
-// options.forEach(function(el,index) {
-//   let option = document.createElement("input");
-//   option.type = "radio";
-//   option.value = el;
-//   option.name = 'optionName';
-
-
-//   let list = document.createElement('li')
-
-//   list.innerHTML = el;
-  
-//   theOptions.appendChild(list);
-//   list.appendChild(option)
-// });
-
-// btn.onclick = function(){
-//   options[answer]
-//   document.getE
-// }
+  finalbtn.onclick = loadResult;
+};
